00d2e2e52d6d3ca881628167a1f14ebd41619544
Varun Lodaya <Varun_Lodaya@symantec.com>
2015-06-16 11:05:05 -0700

    Modified the script to add help options and dynamicaly take password

--- log_collector.sh
+++ log_collector.sh
@@ -1,40 +1,117 @@
 #!/bin/bash
 
-InputVariables=("$@")
-user=${InputVariables[0]}
-password=${InputVariables[1]}
-days=${InputVariables[2]}
+######################################################################
+# Template to collect selective logs from required servers
+######################################################################
+
+#Set Script Name variable
+SCRIPT=`basename ${BASH_SOURCE[0]}`
+
+#Initialize variables to default values.
+days=1
+destination=/data2
+log_type=""
 date=`date +"%m-%d-%Y"`
-DEST_DIR=/data/ # Fill in your own destination directory
+#controllers=('pocsdnash20011.prod.symcpe.net', 'pocsdnash20012.prod.symcpe.net', 'pocsdnash20013.prod.symcpe.net', 'pocsdnash20014.prod.symcpe.net', 'pocsdnash20015.prod.symcpe.net')
+controllers=('pocsdnash20012.prod.symcpe.net' 'pocsdnash20013.prod.symcpe.net' 'pocsdnash20014.prod.symcpe.net')
 
-if [ -z $user ]; then
-    echo "Please provide your username with this script.."
-    echo "e.g) ./log_collector.sh <username>"
-    exit
-fi
+#Set fonts for Help.
+NORM=`tput sgr0`
+BOLD=`tput bold`
+REV=`tput smso`
 
-if [ -z $days ]; then
-    echo "No limit specified, collecting last 1 day of logs"
-    days=0
-else
-    echo "Collecting last $days days of logs"
-fi
+#Help function
+function HELP {
+  echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
+  echo "Command line switches are optional. The following switches are recognized."
+  echo "${REV}-t${NORM}  --Time in days you wanna collect logs for. Default is ${BOLD}${days}${NORM}."
+  echo "${REV}-l${NORM}  --Type of logs you want to collect. Default is ${BOLD}All${NORM}."
+  echo "${REV}-d${NORM}  --Destination directory where you want to copy the log. Default is ${BOLD}${destination}${NORM}."
+  echo "${REV}-u${NORM}  --Username for doing all the operations."
+  echo "${REV}-p${NORM}  --User's password."
+  echo "${REV}-k${NORM}  --User's private key. Currently non-functional, for future use."
+  echo -e "${REV}-h${NORM}  --Displays this help message. No further functions are performed."\\n
+  echo -e "Example 1: ${BOLD}$SCRIPT -t 2 -l contrail-api -u foo -p bar${NORM}"
+  echo -e "Example 2: ${BOLD}$SCRIPT -t 1 -l ifmap${NORM}"\\n
+  exit 1
+}
 
-days=$(expr $days - 1)
+### Start getopts code ###
 
-for ((index=3; index < ${#InputVariables[@]}; index++))
-do
-    server=${InputVariables[$index]}
-    directory_name=$server-$date
-    if [ -d /data/$directory_name ]; then
-        sudo rm -rf /data/$directory_name
-    fi
+#Parse command line flags
+#If an option should be followed by an argument, it should be followed by a ":".
+#Notice there is no ":" after "h". The leading ":" suppresses error messages from
+#getopts. This is required to get my unrecognized option code to work.
 
-    ssh_helper.sh $user $password $server $directory_name $days
-
-    # Now copy over the foler locally
-    scp -i /home/$user/.ssh/id_rsa -r $user@$server:$/${directory_name}.tar.gz /data/
-    echo "Done copying $server log"
-
+while getopts :t:l:d:u:p:k:h FLAG; do
+  case $FLAG in
+    t)  #set option "d"
+      days=$OPTARG
+      ;;
+    l)  #set option "l"
+      log_type=$OPTARG
+      ;;
+    d)  #set option "d"
+      destination=$OPTARG
+      ;;
+    u)  #set option "u"
+      user=$OPTARG
+      ;;
+    p)  #set option "p"
+      password=$OPTARG
+      ;;
+    k)  #set option "k"
+      private_key=$(cat $OPTARG)     
+      ;;
+    h)  #show help
+      HELP
+      ;;
+    \?) #unrecognized option - show help
+      echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
+      HELP
+      #If you just want to display a simple error message instead of the full
+      #help, remove the 2 lines above and uncomment the 2 lines below.
+      #echo -e "Use ${BOLD}$SCRIPT -h${NORM} to see the help documentation."\\n
+      #exit 2
+      ;;
+  esac
 done
 
+shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
+
+if [ -z $user ]; then
+    echo "Please provide your user_name."
+    read user;
+fi
+
+if [ -z $password ]; then
+    echo "Please provide your password."
+    stty -echo
+    read password;
+    stty echo
+fi
+
+### End getopts code ###
+echo "Collecting $log_type logs for last $days days with username $user and saving them in $destination directory" 
+
+### Main loop to process files ###
+
+#This is where your main file processing will take place
+for controller in ${controllers[@]};
+do
+    controller_name=$(echo $controller | cut -d'.' -f1)
+    directory_name=$controller_name-$date
+    if [ -d $destination/$directory_name ]; then
+        sudo rm -rf $destination/$directory_name
+    fi
+    /home/varun_lodaya/ssh_helper.sh $controller $user $password $directory_name $days $log_type
+
+    # Now copy over the folder locally
+    sshpass -p $password scp -r $user@$controller:/tmp/${directory_name}.tar.gz $destination
+    #scp -i /home/$user/.ssh/id_rsa -r $user@$controller:/tmp/${directory_name}.tar.gz $destination
+    echo "Done copying $controller_name log"    
+
+done
+### End main loop ###
+
+exit 0
--- ssh_helper.sh
+++ ssh_helper.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/expect
-
-set user [lindex $argv 0]
-set password [lindex $argv 1]
-set host [lindex $argv 2]
+set host [lindex $argv 0]
+set user [lindex $argv 1]
+set password [lindex $argv 2]
 set directory_name [lindex $argv 3]
 set days [lindex $argv 4]
+set log_type [ lindex $argv 5]
 set prompt ":|#|\\\$"
 
 spawn ssh -o ConnectTimeout=5 -o StrictHostKeyChecking=no -q $user@$host
 expect "password:" { send "$password\r" }
-expect "$prompt " { send "sudo su\r" }
-expect "password: " { send "$password\r" }
-expect "$prompt " { send "rm -rf /home/$user/$directory_name\r" }
-expect "$prompt " { send "mkdir -p /home/$user/$directory_name\r" }
-expect "$prompt " { send "find /var/log/contrail/* -mtime $days -exec cp {} /home/$user/$directory_name \\;\r" }
-expect "$prompt " { send "cd ..\r" }
-expect "$prompt " { send "tar -zcvf /home/$user/${directory_name}.tar.gz /home/$user/$directory_name\r" }
-expect "$prompt " { send "exit\r" }
+expect "$ " { send "sudo su\r" }
+expect "password " { send "$password\r" }
+expect "# " { send "rm -rf /tmp/$directory_name\r" } 
+expect "# " { send "mkdir -p /tmp/$directory_name\r" }
+expect "# " { send "find /var/log/contrail/* -mtime -$days -type f \\( -iname \"$log_type*\" ! -iname \"*analytics*\" ! -iname \"*webui*\" ! -iname \"*discover*\" ! -iname \"*nodemgr*\" ! -iname \"*collector*\" \\) -exec cp {} /tmp/$directory_name \\;\r"}
+#expect "# " { send "find /var/log/contrail/* -mtime $days -exec cp {} /tmp/$directory_name \\;\r" }
+expect "# " { send "cd /tmp\r" }
+expect "# " { send "tar -zcvf ${directory_name}.tar.gz ${directory_name}\r" }
+expect "# " { send "exit\r" }
+
+ 
 
